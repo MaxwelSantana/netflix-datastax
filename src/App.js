b08c8d2408d82e9e91060dd81a5dfb6b12e9fc81
Maxwel <maxwelsant96@icloud.com>
2021-10-12 21:34:18 -0300

    genre limit, load more items on page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Section from "./components/Section";
 import "./App.css";
 
 function App() {
+  const genreIncrement = 4;
   const [genres, setGenres] = useState(null);
+  const [limit, setLimit] = useState(genreIncrement);
 
   const fetchData = async () => {
-    const response = await fetch("/.netlify/functions/getGenres");
+    const response = await fetch("/.netlify/functions/getGenres", {
+      method: "POST",
+      body: limit,
+    });
     const responseBody = await response.json();
     setGenres(responseBody.data.reference_list.values);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [limit]);
 
   return (
     <>
@@ -21,6 +26,12 @@ function App() {
         genres.map((genre) => (
           <Section key={genre.value} genre={genre.value} />
         ))}
+      <div
+        className="page-end"
+        onMouseEnter={() => {
+          setLimit(limit + genreIncrement);
+        }}
+      />
     </>
   );
 }
